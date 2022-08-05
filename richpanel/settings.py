512ebb9b455102b542b0174d8d03a9d0b4157195
Prajwal ! <raiprajwal.paul@gmail.com>
2022-08-05 18:41:44 +0530

    deployment changes

--- richpanel/settings.py
+++ richpanel/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$=$ikwz=_++8!fx=w^_+%8%ty@h83fmpdk-v-ile!9*b43tdw8'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ["prajwalrichpanel.herokuapp.com/", "localhost"]
 
 
 # Application definition
@@ -42,8 +42,8 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -127,5 +127,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Stripe API
 
-STRIPE_SECRET_KEY = 'sk_test_51LSIgtSBrqKkJ5WgpUwfIQYOnWm2eHdz6DnORNcmGyZf9xD4WnNuoGpTHQhrRzJeUuj0LN8NFInRcjOfalCMXeXh00IV7nrXsr'
-STRIPE_PUBLISHABLE_KEY = 'pk_test_51LSIgtSBrqKkJ5Wg1nYerXGqGcMaovsY1oOd1YC1mXKhv0u3kMPglp9mOQMhOPhH7RYUswhoydZSMc8j5VV0mMFH00zgEiaO2L'
+STRIPE_SECRET_KEY = os.environ['STRIPE_SECRET_KEY']
+STRIPE_PUBLISHABLE_KEY = os.environ['STRIPE_PUBLISHABLE_KEY']
