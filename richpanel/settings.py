f495672abc26ec71e45a602f036a033106b37df6
Prajwal ! <raiprajwal.paul@gmail.com>
2022-08-05 18:45:02 +0530

    trying to host

--- richpanel/settings.py
+++ richpanel/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["prajwalrichpanel.herokuapp.com/", "localhost"]
+ALLOWED_HOSTS = ["prajwalrichpanel.herokuapp.com", "localhost"]
 
 
 # Application definition
